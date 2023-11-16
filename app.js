c7fc3244eeb4965891aa5bad38415232bec10b46
Jamienorthman <mikoann88@gmail.com>
2023-11-17 00:37:24 +0100

    saves movies to local storage on click

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 const searchBtn = document.getElementById('search-btn')
 const searchField = document.getElementById('search-field')
 const searchList = document.getElementById('search-list')
-const watchList = document.getElementById('watch-list')
+
 let searchedMovies = []
 let myMovies = []
 
@@ -15,60 +15,54 @@ async function handleSearchClick() {
     const response = await fetch(`http://www.omdbapi.com/?apikey=bd62154e&s=${searchField.value}&type=movie`)
     const data = await response.json()
     console.log(data)
-    searchList.innerHTML = ``
-    for (let movie of data.Search) {
-        const response = await fetch(`http://www.omdbapi.com/?apikey=bd62154e&i=${movie.imdbID}&type=movie`)
-        const data = await response.json()
-        searchList.innerHTML += `
-        <div class="movie-container>
-            <img class="poster" src="${data.Poster}" />
-            <div class="info1>
-                <h4 class="movie-title">${data.Title}</h4>
-                <p class="rating">${data.imdbRating}</p>  
-            </div>
-            <div class="info2>
-                <p class="run-time">${data.Runtime}</p>
-                <p class="genre">${data.Genre}</p>
-                <button class="add-btn" data-add="${data.imdbID}">+ Watchlist</button>
-            </div>
-            <p class="plot">${data.Plot}</p>
-        </div>
-        `
-        searchedMovies.push(data)
-    }
-}
-
-console.log(searchedMovies)
-
-
-function handleAddClick(movieData) {
-    const movieObj = searchedMovies.filter((movie) => {
-        return movie.imdbID == movieData
-    })[0]
-    myMovies.push(movieObj)
-    console.log(myMovies)
-}
-
-const addBtn = document.querySelector(".add-btn")
-addBtn.addEventListener('click', (e) => {
-    handleAddClick(e.target.dataset.add)
-    localStorage.setItem("myMovies", JSON.stringify(myMovies))
-    let storedMovies = JSON.parse(localStorage.getItem("myMovies"))
-    for (let movie of storedMovies) {
-        watchList.innerHTML = `
+    if (data.Search) {
+        searchList.innerHTML = ``
+        for (let movie of data.Search) {
+            const response = await fetch(`http://www.omdbapi.com/?apikey=bd62154e&i=${movie.imdbID}&type=movie`)
+            const data = await response.json()
+            searchedMovies.push(data)
+            searchList.innerHTML += `
             <div class="movie-container>
-            <img class="poster" src="${movie.Poster}" />
-            <div class="info1>
-                <h4 class="movie-title">${movie.Title}</h4>
-                <p class="rating">${movie.imdbRating}</p>  
+                <img class="poster" src="${data.Poster}" />
+                <div class="info1>
+                    <h4 class="movie-title">${data.Title}</h4>
+                    <p class="rating">${data.imdbRating}</p>  
+                </div>
+                <div class="info2>
+                    <p class="run-time">${data.Runtime}</p>
+                    <p class="genre">${data.Genre}</p>
+                    <button id="add-btn" data-add="${data.imdbID}">+ Watchlist</button>
+                </div>
+                <p class="plot">${data.Plot}</p>
             </div>
-            <div class="info2>
-                <p class="run-time">${movie.Runtime}</p>
-                <p class="genre">${movie.Genre}</p>
-                <button class="add-btn" data-remove="${movie.imdbID}">- Remove from Watchlist</button>
-            </div>
-            <p class="plot">${movie.Plot}</p>
-        </div>
             `
+        }
+    } else {
+        searchList.innerHTML = `
+        <p class="unsuccessful">Unable to find what you're looking for. Please try another search.</p>
+        `
+    }
+    
+}
+
+document.addEventListener('click', (e) => {
+    if (e.target.dataset.add) {
+        handleAddClick(e.target.dataset.add)
     }
 })
+
+function handleAddClick(movieID) {
+    const movieObj = searchedMovies.filter((movie) => {
+        return movie.imdbID == movieID
+    })[0]
+    let storedMovies = JSON.parse(localStorage.getItem("myMovies"))
+    if (storedMovies) {
+        myMovies = storedMovies
+    }
+    myMovies.push(movieObj)
+    console.log("test", myMovies)
+    localStorage.setItem("myMovies", JSON.stringify(myMovies))
+}
+
+
+    
