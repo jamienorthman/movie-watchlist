396aecf5eb2ec43bbeaafb0ee71757f203763fb4
Jamienorthman <mikoann88@gmail.com>
2023-11-20 20:46:16 +0100

    add/remove buttons have disabled class, placeholder img for movies with no poster

---
+++ watchlist.js
@@ -0,0 +1,66 @@
+const watchList = document.getElementById('watch-list')
+let storedMovies = JSON.parse(localStorage.getItem('myMovies'))
+let watchListArray = []
+
+    
+document.addEventListener('click', (e) => {
+    if (e.target.dataset.remove) {
+        handleRemoveClick(e.target.dataset.remove)
+    }
+})
+
+if (storedMovies) {
+    watchListArray = storedMovies
+}
+    
+function updateWatchlist() {
+    if (watchListArray) {
+        watchList.innerHTML = ``
+        for (let movie of watchListArray) {
+            watchList.innerHTML += `
+            <div class="movie-container">
+                <img class="poster" src="${movie.Poster}" />
+                <div class="info-container">
+                    <div class="info1">
+                        <h4 class="movie-title">${movie.Title}</h4>
+                        <p class="rating">
+                            <img src="./assets/star.png" class="star-icon">${movie.imdbRating}
+                        </p>  
+                    </div>
+                    <div class="info2">
+                        <p class="run-time">${movie.Runtime}</p>
+                        <p class="genre">${movie.Genre}</p>
+                        <button id="remove-btn" data-remove="${movie.imdbID}">
+                            <img src="./assets/remove.png" class="remove-icon"> Remove
+                        </button>
+                    </div>
+                    <p class="plot">${movie.Plot}</p>
+                </div>
+            </div>
+                ` 
+        }
+    } else {
+        watchList.innerHTML += `
+        <div class="empty-list">
+            <p class="unsuccessful">Your watchlist is looking a little empty...</p>
+            <button>
+                <a href="index.html>
+                    <img src="./assets/add.png"> Let's add some movies!
+                </a>
+            </button>
+        </div>
+        `
+    }
+}
+
+function handleRemoveClick(movieID) {
+    const filteredMovies = watchListArray.filter((movie) => {
+        return movie.imdbID !== movieID
+    })
+    watchListArray = filteredMovies
+    localStorage.setItem('myMovies', JSON.stringify(watchListArray))
+    console.log(watchListArray)
+    updateWatchlist()
+}
+
+updateWatchlist()
